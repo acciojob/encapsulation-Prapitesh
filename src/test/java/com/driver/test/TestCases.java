4b90dd5a99b3e9907967519f9f55bddea1de8f0c
Prapitesh <aitipamulaprapitesh02@gmail.com>
2025-11-28 14:46:05 +0530

    encapsulation

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,8 +1,14 @@
 package com.driver.test;
 
-import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertTrue;
+
 public class TestCases {
-    
+
+    @Test
+    public void dummyTest(){
+        // Dummy test to prevent maven test-compile failure if needed
+        assertTrue(true);
+    }
 }
